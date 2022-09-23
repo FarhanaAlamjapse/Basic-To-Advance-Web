fcee8fa585f6d9c1d27fbc1ae180ef6039be2d10
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-09-24 01:07:22 +0600

    scope-js

--- AdvancedJs/scriptA.js
+++ AdvancedJs/scriptA.js
@@ -1,5 +1,29 @@
 //scope
-var b = "Can I access this?";
-function bb() {
-  var b = "hello";
+// var b = "Can I access this?";
+// function bb() {
+//   var b = "hello";
+// }
+
+var fun = 3;
+function funFunction() {
+  // child scope
+  var fun = "Hello";
+  console.log(1, fun);
 }
+
+function funnerFunction() {
+  // child scope
+  var fun = "Bye";
+  console.log(2, fun);
+}
+
+function funnestFunction() {
+  // child scope
+  fun = "AHHHHH";
+  console.log(3, fun);
+}
+
+console.log("window", fun);
+console.log(funFunction());
+console.log(funnerFunction());
+console.log(funnestFunction());
